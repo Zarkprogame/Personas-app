4560e83697d10bda517890c088796db627e7c811
Zarkprogame <zarkprogame@gmail.com>
2024-03-31 10:53:47 -0500

    Commune Edit

--- resources/views/comuna/edit.blade.php
+++ resources/views/comuna/edit.blade.php
@@ -8,31 +8,38 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    <title>Add Comune</title>
+    <title>Edit Comune</title>
   </head>
   <body>
     <div class="container">
-        <h1>Add Comune</h1>
-        <form method="POST" action="{{ route('comunas.store') }}">
+        <h1>Edit Comune</h1>
+        <form method="POST" action="{{ route('comunas.update', ['comuna' => $comuna->comu_codi]) }}">
+            @method('put')    
             @csrf
             <div class="mb-3">
-                <label for="id" class="form-label">Code</label>
-                <input type="text" class="form-control" id="id" aria-describedby="idHelp" name="id" disabled="disabled">
-                <div id="idHelp" class="form-text">Comune Code</div>
+                <label for="codigo" class="form-label">Id</label>
+                <input type="text" class="form-control" id="id" aria-describedby="codigoHelp" name="id" disabled="disabled" value="{{ $comuna->comu_codi }}">
+                <div id="codigoHelp" class="form-text">Comune Id</div>
             </div>
+
             <div class="mb-3">
                 <label for="name" class="form-label">Comune</label>
-                <input type="text" class="form-control" id="name" aria-describedby="nameHelp" name="name" placeholder="Comuna name.">
+                <input type="text" required class="form-control" id="name" name="name" placeholder="Comuna name" value="{{ $comuna->comu_nomb }}">
             </div>
+
             <label for="municipality">Municipality</label>
             <select class="form-select" id="municipality" name="code" required>
                 <option selected disabled value="">Choose One...</option>
                 @foreach ($municipios as $municipio)
-                    <option value="{{ $municipio->muni_codi }}"> {{ $municipio->muni_nomb }} </option>
+                    @if ($municipio->muni_codi == $comuna->muni_codi)
+                        <option selected value="{{ $municipio->muni_codi }}"> {{ $municipio->muni_nomb }} </option>
+                    @else
+                        <option value="{{ $municipio->muni_codi }}"> {{ $municipio->muni_nomb }} </option>   
+                    @endif    
                 @endforeach
             </select>
             <div class="mt-3">
-                <button type="submit" class="btn btn-primary">Save</button>
+                <button type="submit" class="btn btn-primary">Update</button>
                 <a href="{{ route('comunas.index') }}" class="btn btn-warning">Cancel</a>
             </div>
             </form>
