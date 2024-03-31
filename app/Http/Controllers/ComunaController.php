5fd545eee94d553ad053e25b906e40dcc9583a66
Zarkprogame <zarkprogame@gmail.com>
2024-03-30 19:31:43 -0500

    Ajuste de la vista del listado de comunas

--- app/Http/Controllers/ComunaController.php
+++ app/Http/Controllers/ComunaController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Comuna;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ComunaController
 {
@@ -13,7 +14,10 @@ class ComunaController
      */
     public function index()
     {
-        $comunas = Comuna::all();
+        $comunas = DB::table('tb_comuna')
+        ->join('tb_municipio', 'tb_comuna.muni_codi', '=', 'tb_municipio.muni_codi')
+        ->select('tb_comuna.*', 'tb_municipio.muni_nomb')
+        ->get();
         return view('comuna.index', ['comunas' => $comunas]);
     }
 
