da9d2c5d6c76ffaabff8574602982d398b0afdbd
Zarkprogame <zarkprogame@gmail.com>
2024-03-30 20:09:27 -0500

    Commune Add

--- resources/views/comuna/index.blade.php
+++ resources/views/comuna/index.blade.php
@@ -15,6 +15,7 @@
   <body>
     <div class='container'>
         <h1>Comune List</h1>
+        <a href="{{ route('comunas.create') }}" class="btn btn-success">Add</a>
         <table class='table'>
             <thead>
                 <tr>
